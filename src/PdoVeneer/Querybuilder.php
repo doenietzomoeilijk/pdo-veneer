2078663476b22cf6e73c28afbaa5c86f6f9c1022
Max Roeleveld <doenietzomoeilijk@gmail.com>
2014-09-05 17:27:50 +0200

    More tests, more functionality.

--- src/PdoVeneer/Querybuilder.php
+++ src/PdoVeneer/Querybuilder.php
@@ -16,32 +16,76 @@ class Querybuilder
     /**
      * An array of query parts.
      *
-     * @var array $queryParts
+     * @var array $parts
      */
-    protected $queryParts = array();
+    protected $parts;
+
+    public function __construct()
+    {
+        $this->reset();
+    }
 
     /**
-     * @return PdoVeneer\Querybuilder $this
+     * Does the heavy adding-lifting. Yes, you've guessed it right, you can
+     * abuse my laziness to have a query with "limit 10 AS yourmom".o
+     *
+     * @param string $type one of the keys in the $parts array
+     * @param string|array $toAdd string of one or array of alias => field
+     * @param string $alias
      */
-    public function select()
+    private function addGeneric($type, $toAdd, $alias = null)
     {
+        if (is_array($toAdd)) {
+            foreach ($toAdd as $key => $value) {
+                $this->addGeneric($type, $value, $key);
+            }
+        } else {
+            if ($alias !== null && !is_int($alias)) {
+                 $toAdd .= " AS $alias";
+            }
+
+            $this->parts[$type][] = $toAdd;
+        }
+    }
+
+    /**
+     * Reset the query.
+     *
+     * @return Querybuilder $this
+     */
+    public function reset()
+    {
+        $this->parts = [
+            "select" => [],
+            "from" => [],
+            "flags" => [],
+            "join" => [],
+            "where" => [],
+            "group" => [],
+            "having" => [],
+            "order" => [],
+        ];
+
         return $this;
     }
 
     /**
-     * @param string $table name
+     * @return PdoVeneer\Querybuilder $this
+     */
+    public function select($select, $alias = null)
+    {
+        $this->addGeneric("select", $select, $alias);
+        return $this;
+    }
+
+    /**
+     * @param string|array $table name or array of alias => table pairs.
      * @param string $alias
      * @return PdoVeneer\Querybuilder $this
      */
     public function from($table, $alias = null)
     {
-        $part = $table;
-        if ($alias !== null) {
-            $part .= " AS $alias";
-        }
-
-        $this->queryParts["from"] = $part;
-
+        $this->addGeneric("from", $table, $alias);
         return $this;
     }
 
@@ -56,7 +100,15 @@ class Querybuilder
     /**
      * @return PdoVeneer\Querybuilder $this
      */
-    public function groupBy()
+    public function groupBy($groups)
+    {
+        return $this;
+    }
+
+    /**
+     * @return PdoVeneer\Querybuilder $this
+     */
+    public function limit($groups)
     {
         return $this;
     }
@@ -68,6 +120,19 @@ class Querybuilder
      */
     public function __toString()
     {
-        return "";
+        $query = ["SELECT"];
+
+        if (!empty($this->parts["select"])) {
+            $query[] = implode(", ", (array) $this->parts["select"]);
+        } else {
+            // You really shouldn't do this, but who am I to cramp your style...
+            $query[] = "*";
+        }
+
+        if (!empty($this->parts["from"])) {
+            $query[] = "FROM " . implode(", ", $this->parts['from']);
+        }
+
+        return implode(" ", $query);
     }
 }
--- tests/PdoVeneer/Tests/QuerybuilderTest.php
+++ tests/PdoVeneer/Tests/QuerybuilderTest.php
@@ -6,11 +6,46 @@ use \Dnzm\PdoVeneer\Querybuilder;
 
 class QuerybuilderTest extends \PHPUnit_Framework_TestCase
 {
-    public function testReturnTypes ()
+    public function testReturnTypes()
     {
         $query = new QueryBuilder;
 
         $this->assertInstanceOf("Dnzm\PdoVeneer\Querybuilder", $query);
         $this->assertInternalType("string", (string) $query);
+    }
+
+    public function testSelect()
+    {
+        $query = new Querybuilder;
+
+        // Selecting nothing should result in *.
+        $this->assertEquals("SELECT *", (string) $query);
+
+        // Selecting a single column.
+        $query->reset()
+            ->select("column");
+        $this->assertEquals("SELECT column", (string) $query);
+    }
+
+    public function testFrom()
+    {
+        $query = new Querybuilder;
+
+        // Simple from.
+        $query->from("mytable");
+        $this->assertEquals("SELECT * FROM mytable", (string) $query);
+
+        // From with alias.
+        $query->reset()
+            ->from("mytable", "m");
+        $this->assertEquals("SELECT * FROM mytable AS m", (string) $query);
+
+        // Multiple FROMs in one go.
+        $query->reset()
+            ->from([
+                "mytable",
+                "myothertable"
+            ]);
+        $this->assertEquals("SELECT * FROM mytable, myothertable", (string) $query);
     }
 }
